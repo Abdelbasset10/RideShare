45986d519e90a99a61c2bd00b5de568ad98104f8
Abdelbasset10 <basset.rezazi26@gmail.com>
2023-12-27 23:49:15 +0100

    add trajets functionalities

---
+++ server/routes/trajet.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { createTrajet, getCloseTrajets, getAllTrajets, getTrajet, updateTrajet, deleteTrajet} = require("../controllers/trajet");
+
+const router = express.Router();
+
+router.post("/create", createTrajet);
+router.get("/",getAllTrajets)
+router.get("/:id",getTrajet)
+router.get("/close",getCloseTrajets)
+router.patch('/update/:id',updateTrajet)
+router.delete('/delete/:id',deleteTrajet)
+
+
+
+module.exports = router;
