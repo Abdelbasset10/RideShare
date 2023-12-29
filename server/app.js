657b283fb30abb98a5127b07d957e3cf44f5a1a1
Abdelbasset10 <basset.rezazi26@gmail.com>
2023-12-29 20:57:34 +0100

    add authenticate and admin middlewares

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const prisma = require('./utils/prisma')
 const authRoute = require('./routes/auth')
+const userRoute = require('./routes/user')
 const trajetRoute = require('./routes/trajet')
 
 const app = express()
@@ -10,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use('/auth',authRoute)
 app.use('/trajet',trajetRoute)
+app.use('/user',userRoute)
 
 
 const PORT = process.env.PORT || 5000
---
+++ server/controllers/user.js
@@ -0,0 +1,42 @@
+const prisma = require('../utils/prisma')
+
+const updateUser = async (req,res) => {
+    try {
+        const {id} = req.params
+
+        if(!id){
+            return res.status(400).json({message:"User id is required!"})
+        }
+
+        const user = await prisma.user.findUnique({
+            where:{
+                id
+            }
+        })
+
+        if(!user){
+            return res.status(404).json({message:"User does not exists!"})
+        }
+
+        if(id !== req.userId && req.role !=="ADMIN") {
+            return res.status(401).json({message:"You can't modify an account that's not yours!"})
+        }
+
+        const updatedUser = await prisma.user.update({
+            where:{
+                id
+            },
+            data:{
+                ...req.body
+            }
+        })
+
+        return res.status(200).json(updatedUser)
+    } catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
+
+module.exports = {
+    updateUser
+}
