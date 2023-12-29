85e552e110dde3fef4ac8ef42b161dc61b596567
Abdelbasset10 <basset.rezazi26@gmail.com>
2023-12-29 22:35:14 +0100

    user functions

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { updateUser } = require("../controllers/user");
-const {isSignIn} = require("../middleware/index")
+const { updateUser, deleteUser, getUsers, getUser } = require("../controllers/user");
+const {isSignIn, isAdmin} = require("../middleware/index")
 
 const router = express.Router();
 
 router.patch('/update/:id',isSignIn,updateUser)
+router.delete('/delete/:id',isSignIn,deleteUser)
+router.get('/all',isAdmin,getUsers)
+router.get('/:id',isSignIn,getUser)
 
 
 module.exports = router;
