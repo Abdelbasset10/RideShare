593163b2c75ef97093c77f33fbf47e52acef2c20
Abdelbasset10 <basset.rezazi26@gmail.com>
2024-01-02 18:56:27 +0100

    fix error prblm

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 const express = require('express')
+const cors = require('cors')
 
 const prisma = require('./utils/prisma')
 const authRoute = require('./routes/auth')
@@ -8,6 +9,7 @@ const trajetRoute = require('./routes/trajet')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/auth',authRoute)
 app.use('/trajet',trajetRoute)
