60f4151c08b40ce32d03b80b2cff3b8e5561b29e
Abdelbasset10 <basset.rezazi26@gmail.com>
2024-02-01 16:20:27 +0100

    add delete reservation

--- server/routes/reservation.js
+++ server/routes/reservation.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getUserReservations, updateReservations } = require("../controllers/reservation");
+const { getUserReservations, updateReservations, deleteReservation } = require("../controllers/reservation");
 const {isSignIn, isAdmin} = require("../middleware/index")
 
 const router = express.Router();
 
 router.get('/:id',isSignIn,getUserReservations)
 router.patch('/:id',isSignIn,updateReservations)
+router.delete('/:id',isSignIn,deleteReservation)
 
 module.exports = router;
