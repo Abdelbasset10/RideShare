30e21df3abb1c48b99ee0e16160fb6e5fbd0aa38
Douha0312 <155372645+Douha0312@users.noreply.github.com>
2024-02-03 02:14:33 +0100

    before any change check my comment in AdminProfileTrajets

--- client/src/components/Admin/AdminProfileTrajets.tsx
+++ client/src/components/Admin/AdminProfileTrajets.tsx
@@ -8,7 +8,8 @@ import Grid from '../Profile/Grid.tsx';
 
 const AdminProfileTrajets = () => {
   return (
-    /*ce n'est pas le vrai contenu just pour afficher le travail*/
+    /*ce n'est pas le vrai contenu just pour afficher le travail 
+    ce code va etre deplacer a la page de modification de profil d'utilisateur */
     <div className='flex justify-center sm:flex-col lg:flex-row'>
      <ProfilUserBox/>
       <div className="mr-5"></div>
