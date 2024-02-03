46b76553fb007d482b8edda7df6892c2e08d65cd
Douha0312 <155372645+Douha0312@users.noreply.github.com>
2024-02-03 17:53:51 +0100

    recreate Details Trajet + ProfileUser Box + ProfileEdit + Profile Chauffeur Box + make ProfileEdit Page responsive

---
+++ client/src/pages/DetailsTrajet.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Box from "../components/Box.tsx";
+import ProfileChauffeurBox from "../components/Profile/ProfileChauffeurBox.tsx";
+import destionationIcon from "../assets/img/icons/icon_destination.png";
+import Map from "../assets/img/icons/map.png";
+
+import MapInput from "../components/Map/MapInput.tsx"
+
+const DetailsTrajet = () => {
+  return (
+    <div className="bg-beige pb-96 w-full h-full text-center "> 
+
+    <div className="w-8/12 m-auto">
+      <div className="header">
+        <p className='text-orange text-base'>Pour voi plus de détails sur le trajet selectioné</p>
+        <div className="flex justify-center items-start">
+          <h1 className="mr-1 text-bg-green-dark font-bold text-4xl">Détail De Trajet</h1>
+        </div>
+      </div>
+    </div>
+    <div className="mb-20"></div>
+    <div className='flex  justify-center'>
+      <Box  />
+      <div className="mr-60"></div>
+      <ProfileChauffeurBox />
+    </div>
+    <div className="mb-20"></div>
+    <div className='flex justify-center'>
+          <img className='w-[1211px] h-[798px] 'src={Map} alt="Destination" />
+
+     </div>
+
+
+  </div> 
+   )
+}
+export default DetailsTrajet;
